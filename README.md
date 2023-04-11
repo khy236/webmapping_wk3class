06bd60ffe13bafc09c9fbd83d1b2ab50953ac377
khy236 <khy236@nyu.edu>
2023-04-11 10:08:00 -0400

    first draft - just cluster functionality

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Crowdsourced favorite pizza map
+# Rat spottings over time
 
-Crowdsourced [map](https://khy236.github.io/webmapping_wk3class/) of everyone's favorite pizza shops from 4/6/23 class. Created using MapBox.
+Interactive map showing rat spottings over time [map](https://khy236.github.io/webmapping_wk3project/).
--- js/scripts.js
+++ js/scripts.js
@@ -1,232 +1,144 @@
-// pizza dataset
-const pizzaData = [
-    {
-        "first-name": "Chris",
-        "pizza-shop-name": "Ben's Pizza",
-        "place": "the Village",
-        "longitude": -74.000288,
-        "latitude": 40.730412,
-        "program": "MUP"
-    },
-    {
-        "first-name": "Max",
-        "pizza-shop-name": "Lou Malnati's Pizzeria",
-        "place": "Chicago",
-        "longitude": -87.63368899,
-        "latitude": 41.89067478,
-        "program": "MSDS"
-    },
-    {
-        "first-name": "Christina",
-        "pizza-shop-name": "Pie-Eyed Pizzeria",
-        "place": "Chicago",
-        "longitude": -87.65571,
-        "latitude": 41.89619,
-        "program": "MUP"
-    },
-    {
-        "first-name": "Nate",
-        "pizza-shop-name": "Pizza Suprema",
-        "place": "Midtown",
-        "longitude": -73.995163,
-        "latitude": 40.75063,
-        "program": "MSPP"
-    },
-    {
-        "first-name": "Patrick",
-        "pizza-shop-name": "Patsy's",
-        "place": "East Harlem",
-        "longitude": -73.93486712,
-        "latitude": 40.797137,
-        "program": "MUP"
-    },
-    {
-        "first-name": "Evan",
-        "pizza-shop-name": "Rosa's",
-        "place": "Ridgewood",
-        "longitude": -73.900092,
-        "latitude": 40.712197,
-        "program": "MUP"
-    },
-    {
-        "first-name": "Aisha",
-        "pizza-shop-name": "Christian's Pizza",
-        "place": "Charlottesville",
-        "longitude": -78.48223019,
-        "latitude": 38.0310708,
-        "program": "MUP"
-    },
-    {
-        "first-name": "Luke",
-        "pizza-shop-name": "Pica's ",
-        "place": "Philadelphia",
-        "longitude": -75.27540655,
-        "latitude": 39.96583696,
-        "program": "MUP"
-    },
-    {
-        "first-name": "Ritwick",
-        "pizza-shop-name": "Percy's",
-        "place": "Greenwich Village",
-        "longitude": -74.00138792,
-        "latitude": 40.72933278,
-        "program": "MUP"
-    },
-    {
-        "first-name": "Whitney",
-        "pizza-shop-name": "Piz-zetta ",
-        "place": "Downtown Brooklyn",
-        "longitude": -73.9907819,
-        "latitude": 40.69243875,
-        "program": "EMPA"
-    },
-    {
-        "first-name": "Becky",
-        "pizza-shop-name": "Prince Street Pizza",
-        "place": "SOHO",
-        "longitude": -73.99454946,
-        "latitude": 40.72388084,
-        "program": "ITP"
-    },
-    {
-        "first-name": "Yu Ze",
-        "pizza-shop-name": "Mama's TOO!",
-        "place": "Upper West Side",
-        "longitude": -73.96760083,
-        "latitude": 40.80101105,
-        "program": "CUSP"
-    },
-    {
-        "first-name": "Anna ",
-        "pizza-shop-name": "John's of Bleecker ",
-        "place": "Greenwich Village ",
-        "longitude": -74.00337035,
-        "latitude": 40.73170071,
-        "program": "CUSP"
-    },
-    {
-        "first-name": "Camille ",
-        "pizza-shop-name": "John's of Bleecker ",
-        "place": "Greenwich Village ",
-        "longitude": -74.00337035,
-        "latitude": 40.73170071,
-        "program": "MUP"
-    },
-    {
-        "first-name": "Bria",
-        "pizza-shop-name": "San Matteo",
-        "place": "Upper East Side",
-        "longitude": -73.95487,
-        "latitude": 40.77398,
-        "program": "MPA"
-    },
-    {
-        "first-name": "Chloe",
-        "pizza-shop-name": "Village Square Pizza",
-        "place": "East Village",
-        "longitude": -73.98279206,
-        "latitude": 40.72768156,
-        "program": "MSDS"
-    },
-    {
-        "first-name": "Karen",
-        "pizza-shop-name": "Annie's Italian restaurant",
-        "place": "Beijing",
-        "longitude": 116.43731,
-        "latitude": 39.91891,
-        "program": "MUP"
-    }
-]
-
+/* ADAPTED FROM:
+ 1. Show changes over time with Mapbox GL JS: https://docs.mapbox.com/help/tutorials/show-changes-over-time/ 
+ 2. Create and style clusters: https://docs.mapbox.com/mapbox-gl-js/example/cluster/ */
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2h5MjM2IiwiYSI6ImNsZzVxYTVnNDA1d2kzZW45b3l5d280N3oifQ.GqfNX5HwLaA5utEN2iQkXg';
 
-const NYC_COORDINATES = [-74.00888, 40.70000]
+const NYC_COORDINATES = [-74.00630, 40.71419] // FiDi
 
+// 
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
-    center: NYC_COORDINATES, // starting position [lng, lat]
-    zoom: 10, // starting zoom
-    pitch: 15
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v11',
+    center: NYC_COORDINATES,
+    zoom: 9.8,
+    pitch: 15,
+    bearing: 0,
+    container: 'map',
+    antialias: true
 });
 
 
-// real markers 
+// 
+map.on('load', () => {
 
-pizzaData.forEach(function (pizzaRecord) {
+    map.addSource('ratsightings', {
+        type: 'geojson',
+        data: 'https://data.cityofnewyork.us/resource/3q43-55fe.geojson', // NYC OpenData 311 rodent complaints
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+    });
 
-    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-        `${pizzaRecord['pizza-shop-name']}`
-    );
+    map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'ratsightings',
+        filter: ['has', 'point_count'],
+        paint: {
+            // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+            // with three steps to implement three types of circles:
+            //   * Blue, 20px circles when point count is less than 100
+            //   * Yellow, 30px circles when point count is between 100 and 750
+            //   * Pink, 40px circles when point count is greater than or equal to 750
+            'circle-color': [
+                'step',
+                ['get', 'point_count'],
+                '#51bbd6',
+                50,
+                '#f1f075',
+                200,
+                '#f28cb1'
+            ],
+            'circle-radius': [
+                'step',
+                ['get', 'point_count'],
+                20,
+                100,
+                30,
+                750,
+                40
+            ]
+        }
+    });
 
-    // marker color based on program
-    if (pizzaRecord.program === 'MUP') {
-        color = '#FC2F03'
-    } else {
-        color = '#ffffff'
-    }
+    map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'ratsightings',
+        filter: ['has', 'point_count'],
+        layout: {
+            'text-field': ['get', 'point_count_abbreviated'],
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12
+        }
+    });
+
+    map.addLayer({
+        id: 'unclustered-point',
+        type: 'circle',
+        source: 'ratsightings',
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+            'circle-color': '#11b4da',
+            'circle-radius': 4,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#fff'
+        }
+    });
+
+    // inspect a cluster on click
+    map.on('click', 'clusters', (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+            layers: ['clusters']
+        });
+        const clusterId = features[0].properties.cluster_id;
+        map.getSource('ratsightings').getClusterExpansionZoom(
+            clusterId,
+            (err, zoom) => {
+                if (err) return;
+
+                map.easeTo({
+                    center: features[0].geometry.coordinates,
+                    zoom: zoom
+                });
+            }
+        );
+    });
+
+    // When a click event occurs on a feature in
+    // the unclustered-point layer, open a popup at
+    // the location of the feature, with
+    // description HTML from its properties.
+    map.on('click', 'unclustered-point', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const loc = e.features[0].properties.incident_address;
+        const status = e.features[0].properties.status;
+        const created = e.features[0].properties.created_date;
+        const closed = e.features[0].properties.closed_date;
+        
+        // Ensure that if the map is zoomed out such that
+        // multiple copies of the feature are visible, the
+        // popup appears over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(
+                `Location: ${loc}<br>Status: ${status}<br>Date created: ${created}<br>Date closed: ${closed}`
+            )
+            .addTo(map);
+    });
+
+    map.on('mouseenter', 'clusters', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    map.on('mouseleave', 'clusters', () => {
+        map.getCanvas().style.cursor = '';
+    });
+});
 
 
-    // marker for gowanus whole foods (GWF)
-    new mapboxgl.Marker({
-        color: color
-    })
-        .setLngLat([pizzaRecord.longitude, pizzaRecord.latitude])
-        .setPopup(popup) // sets popup for marker
-        .addTo(map);
-})
 
-
-$('#fly-to-beijing').on('click', function () {
-    map.flyTo({
-        center: [116.43731, 39.91891]
-    })
-})
-
-$('#fly-to-chicago').on('click', function () {
-    map.flyTo({
-        center: [-87.6298, 41.8781]
-    })
-})
-
-
-$('#fly-to-philadelphia').on('click', function () {
-    map.flyTo({
-        center: [-75.27540655, 39.96583696]
-    })
-})
-
-$('#fly-to-charlottesville').on('click', function () {
-    map.flyTo({
-        center: [-78.4822301, 38.0310708]
-    })
-})
-
-$('#fly-to-new-york').on('click', function () {
-    map.flyTo({
-        center: [-74.000288, 40.73041]
-    })
-})
-
-
-/* DUMMY MARKERS
-
-// map center point
-const marker1 = new mapboxgl.Marker()
-    .setLngLat(NYC_COORDINATES)
-    .addTo(map);
-
-// create popup for GWF marker
-const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-    'Gowanus Whole Foods'
-);
-
-// marker for gowanus whole foods (GWF)
-    const marker2 = new mapboxgl.Marker()
-    .setLngLat(GOWANUS_WHOLEFOODS)
-    .setPopup(popup) // sets popup for marker
-    .addTo(map);
-
-    */
